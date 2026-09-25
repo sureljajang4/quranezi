613bed6efb48286f2c73345ca0e0b004ea42e2fa
sureljajang4 <sureljajang4@gmail.com>
2026-09-25 11:10:39 +0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'blogger-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/favicon.ico'
+];
+
+// Instasiasi Service Worker dan simpan cache dasar
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Aktivasi Service Worker dan bersihkan cache lama jika ada update
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Ambil data dari cache jika offline, atau ambil dari internet jika online
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
